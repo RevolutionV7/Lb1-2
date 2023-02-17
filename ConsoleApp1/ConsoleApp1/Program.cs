330ef2cbcb3b2ae5f0ad644aa40ff71ee8bb2e11
Dima <sdvsvd11@gmail.com>
2023-02-17 18:51:54 +0200

    finish

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public delegate int Delegat(int a, int b);
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть 3 цілі числа.");
+            string a = Console.ReadLine();
+            Console.WriteLine("а = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b = ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("c = ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            {
+                case "+":
+                    Delegat Delegat = (c, b, a) => { return c + b + a; };
+                    int del = Delegat(x, y, z);
+                    Console.WriteLine("a+b+c = ", del);
+                    break;
+                default:
+                    Console.WriteLine("Недоступна операцiя");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        delegate int MyDelegate();
+        delegate double MyDel(MyDelegate[] a);
+
+        static int GetRandom()
+        {
+            return new Random().Next(1, 10);
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Введiть кiлькiсть елементiв масива: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('-', 100));
+
+            var array = new MyDelegate[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = () => new MyDelegate(GetRandom)();
+            }
+
+            MyDel d = delegate (MyDelegate[] a)
+            {
+                double sr = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    sr += a[i]();
+                }
+                return sr / array.Length;
+            };
+
+            Console.WriteLine("Среднє арифметичне = {0}", d(array));
+            Console.WriteLine(new string('-', 100));
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]());
+            }
+        }
+    }
